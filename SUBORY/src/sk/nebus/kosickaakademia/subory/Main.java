25bedcbc06750ff845e568bfd1042bc96c397440
Branislav Nebus <branislav.nebus@kosickaakademia.sk>
2020-11-11 13:52:25 +0100

    has been added new methods

--- SUBORY/src/sk/nebus/kosickaakademia/subory/Main.java
+++ SUBORY/src/sk/nebus/kosickaakademia/subory/Main.java
@@ -3,6 +3,14 @@ package sk.nebus.kosickaakademia.subory;
 public class Main {
     public static void main(String[] args) {
     RWFile rwFile = new RWFile();
+    //urobi novy tex editor s velkymi pismenami
     rwFile.copyFileWithCapital("resource/a.txt");
+
+    //urobi novy text editor so sifrou
+    rwFile.createFileCesar("resource/a.txt",-5);
+
+    //urobi novy text editor a kde je i alebo y da _
+    rwFile.createFileWithoutIorY("resource/a.txt");
+
     }
 }
--- SUBORY/src/sk/nebus/kosickaakademia/subory/RWFile.java
+++ SUBORY/src/sk/nebus/kosickaakademia/subory/RWFile.java
@@ -17,7 +17,7 @@ public class RWFile {
         try {
             fr = new FileReader(nameFile);
             reader = new BufferedReader(fr);
-            File file = new File("output/" + getFileName());
+            File file = new File("output/" + getFileName('b'));
             file.createNewFile();
             fw = new FileWriter(file);
 
@@ -34,17 +34,165 @@ public class RWFile {
 
     }
 
-
-    private String getFileName() {
+    private String getFileName(char preffix) {
         Calendar calendar = Calendar.getInstance();
         Date cas = calendar.getTime();
 
-        DateFormat dateFormat = new SimpleDateFormat("hhmmss");
+        DateFormat dateFormat = new SimpleDateFormat("HHmmss");
         String retazec2 = dateFormat.format(cas);
 
-        String retazec1 = "b_";
         String retazec3 = ".txt";
-        return retazec1+retazec2+retazec3;
+        if(preffix == 'b'){
+            return "b_"+retazec2+retazec3;
+        }else if(preffix == 'd'){
+            return "d_"+retazec2+retazec3;
+        }else if(preffix == 'c'){
+            return "c_"+retazec2+retazec3;
+        }
+        return null;
+    }
+
+    public void createFileCesar(String nameFile, int offset){
+        try{
+            //objekt suboru
+            FileReader fr = new FileReader(nameFile);
+            //objekt suboru
+            File file = new File("output/" + getFileName('c'));
+            //citac
+            BufferedReader br = new BufferedReader(fr);
+            //vytvorenie suboru
+            file.createNewFile();
+
+            String line;
+            String retazec = "";
+
+            while ((line = br.readLine()) != null){
+                retazec = retazec + line;
+                retazec = retazec + '\n';
+            }
+            fr.close();
+            //zapisovac
+            FileWriter fw = new FileWriter(file);
+
+            //dame do pola cely string
+            char[] pole = new char[retazec.length()];
+            //naplnime cez for pole
+            for(int i = 0; i < retazec.length(); i++){
+                pole[i] = retazec.charAt(i);
+            }
+            //zasifrujem pole
+            //ked bude offset plusovy
+            if(offset > 0) {
+                for (int i = 0; i < retazec.length(); i++) {
+                    if (pole[i] >= 'A' && pole[i] <= 'Z') {
+                        for (int j = 1; j <= offset; j++) {
+                            pole[i] = (char) (pole[i] + 1);
+                            if (pole[i] > 'Z') {
+                                pole[i] = 'A';
+                            }
+                        }
+                    } else if (pole[i] >= 'a' && pole[i] <= 'z') {
+                        for (int j = 1; j <= offset; j++) {
+                            pole[i] = (char) (pole[i] + 1);
+                            if (pole[i] > 'z') {
+                                pole[i] = 'a';
+                            }
+                        }
+                    } else if (pole[i] >= '0' && pole[i] <= '9') {
+                        for (int j = 1; j <= offset; j++) {
+                            pole[i] = (char) (pole[i] + 1);
+                            if (pole[i] > '9') {
+                                pole[i] = '0';
+                            }
+                        }
+                    } else {
+                        pole[i] = pole[i];
+                    }
+                }
+            }else{
+                //ked bude offset minusovi
+                for (int i = 0; i < retazec.length(); i++) {
+                    if (pole[i] >= 'A' && pole[i] <= 'Z') {
+                        for (int j = 1; j <= offset*(-1); j++) {
+                            pole[i] = (char) (pole[i] - 1);
+                            if (pole[i] < 'A') {
+                                pole[i] = 'Z';
+                            }
+                        }
+                    } else if (pole[i] >= 'a' && pole[i] <= 'z') {
+                        for (int j = 1; j <= offset*(-1); j++) {
+                            pole[i] = (char) (pole[i] - 1);
+                            if (pole[i] < 'a') {
+                                pole[i] = 'z';
+                            }
+                        }
+                    } else if (pole[i] >= '0' && pole[i] <= '9') {
+                        for (int j = 1; j <= offset*(-1); j++) {
+                            pole[i] = (char) (pole[i] - 1);
+                            if (pole[i] < '0') {
+                                pole[i] = '9';
+                            }
+                        }
+                    } else {
+                        pole[i] = pole[i];
+                    }
+                }
+            }
+
+            //pole nakopirujeme do suboru
+            for(int i = 0; i < pole.length; i++){
+                    fw.write(pole[i]);
+            }
+            fw.close();
+
+        }catch(IOException ex){
+            System.out.println("ERROR: " + ex);
+        }
+
+    }
+
+    public void createFileWithoutIorY(String nameFile){
+        try{
+            //objekt suboru
+            FileReader fr = new FileReader(nameFile);
+            //objekt suboru
+            File file = new File("output/" + getFileName('d'));
+            //citac
+            BufferedReader br = new BufferedReader(fr);
+            //vytvorenie suboru
+            file.createNewFile();
+
+            String line;
+            String retazec = "";
+
+            while ((line = br.readLine()) != null){
+                retazec = retazec + line;
+                retazec = retazec + '\n';
+            }
+            fr.close();
+            //zapisovac
+            FileWriter fw = new FileWriter(file);
+
+            //dame do pola cely string
+            char[] pole = new char[retazec.length()];
+            //naplnime cez for
+            for(int i = 0; i < retazec.length(); i++){
+                pole[i] = retazec.charAt(i);
+            }
+            //pole nakopirujeme do suboru bez i alebo y
+            for(int i = 0; i < pole.length; i++){
+                if(pole[i] == 'i' || pole[i] == 'y'){
+                    fw.write('_');
+                }else{
+                    fw.write(pole[i]);
+                }
+            }
+            fw.close();
+
+        }catch(IOException ex){
+            System.out.println("ERROR: " + ex);
+        }
+
     }
 }
 
